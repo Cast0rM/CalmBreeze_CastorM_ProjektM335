254c959876e15058eabfa3fa02e9451a43dc4498
Cast0rM <113532532+Cast0rM@users.noreply.github.com>
2024-03-19 08:33:03 +0100

    MainExperience: addet Countdown

--- App/app/src/main/java/com/example/calmbreeze_m335_castorm/Experience.java
+++ App/app/src/main/java/com/example/calmbreeze_m335_castorm/Experience.java
@@ -1,6 +1,8 @@
 package com.example.calmbreeze_m335_castorm;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Experience extends AppCompatActivity {
-
+    private TextView countdownText;
+    private long timeMs;
+    private final Handler handler = new Handler();
+    private final long Countdown_Duration_MS = 1 * 60 * 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,48 @@ public class Experience extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        countdownText = findViewById(R.id.countdown);
+        timeMs = System.currentTimeMillis();
+        startCountdown();
+    }
+
+    private void startCountdown() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsedTimeMs = System.currentTimeMillis() - timeMs;
+                long remainingTimeMs = Countdown_Duration_MS - elapsedTimeMs;
+                updateCountdown(remainingTimeMs);
+                if (remainingTimeMs > 0) {
+                    handler.postDelayed(this, 1000); // Update countdown every second
+                } else {
+                    // TODO Vibration && Popup || Benachrichtigung
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("timeMs", timeMs);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        timeMs = savedInstanceState.getLong("timeMs");
+        startCountdown();
+    }
+
+    private void updateCountdown(long remainingTimeMs) {
+        int seconds = (int) (remainingTimeMs / 1000);
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        String countdownString = String.format("%02d:%02d", minutes, seconds);
+        countdownText.setText(countdownString);
     }
 }
